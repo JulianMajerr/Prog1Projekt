69c85f278a214529c159e041c050fc4affa3e789
Julian <128976496+JulianMajerr@users.noreply.github.com>
2023-12-19 15:32:10 +0100

    Add files via upload

---
+++ Lager.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Lager
+{
+    
+    ArrayList<int[]> regale = new ArrayList<int[]>();
+    
+    public Lager(){ 
+        int regal1[] = new int[50];
+        regale.add(regal1);
+        
+        
+    }
+}
